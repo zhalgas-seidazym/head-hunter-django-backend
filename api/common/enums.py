b40175e0e6672d44c5e10c9b358e58eb07a0b19f
zhalgas-seidazym <zhalgas.seidazym2005@gmail.com>
2025-07-06 23:33:11 +0500

    added enum for experience

--- api/common/enums.py
+++ api/common/enums.py
@@ -19,8 +19,14 @@ ORGANIZATION_ROLE_RANKS = {
     OrganizationRole.VIEWER: 0,
 }
 
-
 class OrganizationRequestStatus(models.TextChoices):
     PENDING = 'pending', 'Pending'
     ACCEPTED = 'accepted', 'Accepted'
     DECLINED = 'declined', 'Declined'
+
+class WorkExperience(models.TextChoices):
+    NO_EXPERIENCE = "no_experience", "Нет опыта"
+    LESS_THAN_ONE = "less_than_one", "Меньше года"
+    ONE_TO_THREE = "one_to_three", "От 1 года до 3 лет"
+    THREE_TO_FIVE = "three_to_five", "От 3 до 5 лет"
+    MORE_THAN_FIVE = "more_than_five", "Более 5 лет"

14bcd7e790748373bed164e474247d78dee35e2f
zhalgas-seidazym <zhalgas.seidazym2005@gmail.com>
2025-07-08 06:43:44 +0500

    added new enums for vacancy

--- api/common/enums.py
+++ api/common/enums.py
@@ -30,3 +30,32 @@ class WorkExperience(models.TextChoices):
     ONE_TO_THREE = "one_to_three", "От 1 года до 3 лет"
     THREE_TO_FIVE = "three_to_five", "От 3 до 5 лет"
     MORE_THAN_FIVE = "more_than_five", "Более 5 лет"
+
+class EmploymentType(models.TextChoices):
+    FULL_TIME = "FULL_TIME", "Полная занятость"
+    PART_TIME = "PART_TIME", "Частичная занятость"
+    PROJECT = "PROJECT", "Проектная занятость"
+    ROTATIONAL = "ROTATIONAL", "Вахтовый метод"
+
+class WorkFormat(models.TextChoices):
+    ON_SITE = "ON_SITE", "На месте работодателя"
+    REMOTE = "REMOTE", "Удалённо"
+    HYBRID = "HYBRID", "Гибрид"
+    FIELD = "FIELD", "Разъездной"
+
+class WorkSchedule(models.TextChoices):
+    FULL_DAY = "FULL_DAY", "Полный день"
+    SHIFT = "SHIFT", "Сменный график"
+    FLEXIBLE = "FLEXIBLE", "Гибкий график"
+    REMOTE = "REMOTE", "Удалённая работа"
+    ROTATIONAL = "ROTATIONAL", "Вахтовый метод"
+
+class PaymentFrequency(models.TextChoices):
+    HOURLY = "HOURLY", "Почасовая оплата"
+    DAILY = "DAILY", "Ежедневно"
+    WEEKLY = "WEEKLY", "Еженедельно"
+    BIWEEKLY = "BIWEEKLY", "Раз в две недели"
+    MONTHLY = "MONTHLY", "Ежемесячно"
+    QUARTERLY = "QUARTERLY", "Ежеквартально"
+    ANNUALLY = "ANNUALLY", "Ежегодно"
+    ONE_TIME = "ONE_TIME", "Единовременная выплата"
